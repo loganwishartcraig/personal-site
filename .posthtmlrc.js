ed510b1b8dece74df9671bf595672890b066c64b
Logan Wishart-Craig <loganwishartcraig@gmail.com>
2020-01-18 22:45:47 -0800

    Added parcel, postcss, posthtml. Added more content

---
+++ .posthtmlrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"plugins": {
+		"posthtml-doctype": {
+			"doctype": "HTML 5"
+		},
+		"posthtml-extend": {
+			"root": "./src"
+		},
+		"posthtml-inline-svg": {
+			"cwd": process.cwd() + "/src"
+		}
+	}
+};
