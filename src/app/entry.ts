145dda453909dade67301d62309eb81066e6ec13
loganwishartcraig <loganwishartcraig@gmail.com>
2020-01-28 20:46:34 -0800

    Added rough draft nearest neighbor edge implementation

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,7 +1,13 @@
-import { BufferAttribute, BufferGeometry, Color, PerspectiveCamera, Points, Scene, ShaderMaterial, WebGLRenderer, Colors } from 'three';
+import { 
+    BufferAttribute, BufferGeometry, Color, PerspectiveCamera, Points, Scene, ShaderMaterial, WebGLRenderer, Colors,
+    LineBasicMaterial,
+    LineSegments,
+} from 'three';
 
+import {TypedArrayUtils} from './TypedArrayUtils';
 
 const NUM_VERTICES = 250;
+const MAX_EDGE_VERTICES= 600 * 2;
 const MAX_DISTANCE = Math.pow(120, 2);
 
 enum COLOR {
@@ -9,11 +15,15 @@ enum COLOR {
     BG_DARK = 0x000000,
     VERTEX_LIGHT = 0x666666,
     VERTEX_DARK = 0x808080,
+    EDGE_LIGHT = 0xdddddd,
+    EDGE_DARK = 0x222222,
 }
 
 let useDark = true;
 let ih = window.innerHeight,
     iw = window.innerWidth
+
+const distanceFunction = (a, b) => Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2);
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -33,18 +43,25 @@ document.body.appendChild(renderer.domElement);
 camera.position.z = 500;
 
 const positions = new Float32Array(NUM_VERTICES * 3);
+const edgePositions = new Float32Array(MAX_EDGE_VERTICES * 3);
+
 const alphas = new Float32Array(NUM_VERTICES);
-const velocity = new Float32Array(NUM_VERTICES * 2);
+const velocity = new Float32Array(NUM_VERTICES * 2); 
 
 const particleGeom = new BufferGeometry();
 particleGeom.setAttribute('position', new BufferAttribute(positions, 3));
 particleGeom.setAttribute('alpha', new BufferAttribute(alphas, 1));
+
+const edgeGeom = new BufferGeometry();
+edgeGeom.setAttribute('position', new BufferAttribute(edgePositions, 3));
 
 for (let i = 0; i < NUM_VERTICES; i++) {
 
     positions[i * 3 + 0] = 2 * Math.random() * iw - iw;
     positions[i * 3 + 1] = 2 * Math.random() * ih - ih;
     positions[i * 3 + 2] = 0.0;
+
+    const key = `${positions[i*3+0]},${positions[i*3+1]},${positions[i*3+2]}`;
 
     velocity[i * 2 + 0] = (Math.random() > 0.5 ? -1 : 1) * Math.random();
     velocity[i * 2 + 1] = (Math.random() > 0.5 ? -1 : 1) * Math.random();
@@ -63,10 +80,15 @@ const particleMat = new ShaderMaterial({
     fragmentShader: require('./shaders/pointFragment.ts').src,
     transparent: true,
 });
+const edgeMat = new LineBasicMaterial( { color: useDark ? COLOR.EDGE_DARK : COLOR.EDGE_LIGHT } );
 
 const particles = new Points(particleGeom, particleMat);
-scene.add(particles);
+const edges = new LineSegments(edgeGeom, edgeMat); 
+let kdtree;
+let seenEdges;
 
+scene.add(particles);
+scene.add(edges);
 let resizeTimeout;
 
 function handleResize() {
@@ -75,6 +97,8 @@ function handleResize() {
         requestAnimationFrame(() => {
             ih = window.innerHeight;
             iw = window.innerWidth;
+            camera.aspect = iw / ih;
+            camera.updateProjectionMatrix();
             renderer.setSize(iw, ih);
         });
     }, 300);
@@ -96,8 +120,65 @@ function animate() {
 
     }
 
-    particleGeom.attributes.position.needsUpdate = true;
+    // It's inefficient to just rebuild the K-d tree for each
+    // frame, but I don't know a good spatial index that would 
+    // support point animation in this way. We also slice the 
+    // positions buffer to create a copy so the sorting in the kd tree
+    // doesn't mutate the input buffer. Again - inefficient...
+    // would at least be better if we could insert/remove from the kd-tree 
+    // and maintain balance but the TypedArrayUtils implementation does not support this.
+    kdtree = new TypedArrayUtils.Kdtree(positions.slice(), distanceFunction, 3);
+    seenEdges = {};
 
+    let i = 0; 
+    let j = 0;
+
+    while (i < NUM_VERTICES && j < MAX_EDGE_VERTICES) {
+
+        const key = `${positions[i*3+0]},${positions[i*3+1]},${positions[i*3+2]}`;
+        const nearest = kdtree.nearest(
+            [
+                positions[i * 3 + 0],
+                positions[i * 3 + 1],
+                positions[i * 3 + 2],
+            ],
+            100,
+            MAX_DISTANCE
+        );
+        for (let c = 0, nl = nearest.length; c < nl && j < MAX_EDGE_VERTICES; c++) {
+
+            const key_b = `${nearest[c][0].obj[0]},${nearest[c][0].obj[1]},${nearest[c][0].obj[2]}`;
+
+            if ((seenEdges[key] && seenEdges[key][key_b]) || (seenEdges[key_b] && seenEdges[key_b][key])) {
+                continue;
+            }
+
+            seenEdges[key] = seenEdges[key] || {};
+            seenEdges[key][key_b] = true;
+
+            edgePositions[j * 3 + 0] = positions[i * 3 + 0];
+            edgePositions[j * 3 + 1] = positions[i * 3 + 1];
+            edgePositions[j * 3 + 2] = positions[i * 3 + 2];
+
+            edgePositions[(j+1) * 3 + 0] = nearest[c][0].obj[0]; 
+            edgePositions[(j+1) * 3 + 1] = nearest[c][0].obj[1]; 
+            edgePositions[(j+1) * 3 + 2] = nearest[c][0].obj[2]; 
+
+            j+=2;
+
+        }
+        i++;
+    }
+
+    while (j < MAX_EDGE_VERTICES) {
+        edgeGeom[j * 3 + 0] = null;
+        edgeGeom[j * 3 + 1] = null;
+        edgeGeom[j * 3 + 2] = null;
+        j++;
+    }
+
+    (particleGeom.attributes.position as any).needsUpdate = true;
+    (edgeGeom.attributes.position as any).needsUpdate = true;
 
     renderer.render(scene, camera);
 }
@@ -114,6 +195,7 @@ if (typeof window.matchMedia === 'function') {
 function setColors(useDark: boolean) {
     renderer.setClearColor(useDark ? COLOR.BG_DARK : COLOR.BG_LIGHT, 1);
     uniforms.color.value = new Color(useDark ? COLOR.VERTEX_DARK : COLOR.VERTEX_LIGHT);
+    edgeMat.color = new Color(useDark ? COLOR.EDGE_DARK : COLOR.EDGE_LIGHT);
 }
 
 window.addEventListener('resize', handleResize);
